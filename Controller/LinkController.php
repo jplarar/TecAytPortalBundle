ac3106ae0416ccf800c62ee6f28d54420ad15c94
Link-NM <linknmasters@gmail.com>
2015-12-03 00:27:38 -0600

    Create LinkController.php

---
+++ Controller/LinkController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tec\Ayt\PortalBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LinksController extends Controller
+{
+   public function listAction()
+   {
+
+       $documents = $this->getDoctrine()->getRepository('Tec\Ayt\CoreBundle\Entity\Links')->findAll();
+
+
+       $freeLinks = array();
+
+       /** @var \Tec\Ayt\CoreBundle\Entity\Links $Links */
+       foreach ($links as $links) {
+
+               $freeLinsk[] = $links;
+
+       }
+
+       return $this->render('TecAytPortalBundle:Links:list.html.twig', array(
+           'freeLinks' => $freeLinks
+       ));
+   }
+}
